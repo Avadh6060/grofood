a6359782da291aa9f77463ac80769f6abf88a752
AvadhPatel <avadhpatel6060@gmail.com>
2021-08-02 21:23:55 +0530

    frist commit

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container">
-                    <Link className="navbar-brand" to="/"><img src="../images/header_logo.png" className="logo"></img></Link>
+                    <Link className="navbar-brand" to="/"><img src="images/header_logo.png" className="logo"></img></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
